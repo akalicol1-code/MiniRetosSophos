874076589c94e68eb8253e288922711046ae16a8
Cesar Mendoza <cesaraugustomendozayanez@gmail.com>
2022-11-21 21:10:38 -0500

    Modificado otra vez

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Experimentando con ");
+		System.out.println("Modificación final del archivo antes del commit");
 
 	}
 
